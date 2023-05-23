9148ea0dfab5c7c24df5511c860b87044d23596e
kathy yost <kathy.yost@student.centralia.edu>
2023-05-23 05:47:29 -0700

    Add project files.

---
+++ MultiUserBank_Yost/CellPhone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboDialer_yost
+{
+
+        internal class CellPhone : Phone
+        {
+            public CellPhone(string companyName, string phoneNumber, int phoneType) : base(companyName, phoneNumber, phoneType)
+            {
+            }
+
+            public override string Dial()
+            {
+                return $"{CompanyName} is being dialed using 1+{PhoneNumber}...";
+            }
+        }
+    
+}
